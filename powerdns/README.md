3494d0397374cc7fd1254bb68fe93e18d12f5987
Nicolas <nicolas@sd-90634.dedibox.fr>
2018-11-29 17:02:13 +0100

    add systemd for PowerDNS-admin

--- powerdns/README.md
+++ powerdns/README.md
@@ -1,1 +1,27 @@
-# vagrant-poc
+PowerDNS with Vagrant
+==============
+
+Vagrant box with PowerDNS and PowerDNS-admin on CentOS 7.
+
+Only for testing purpose.
+
+# Requirements
+
+* Vagrant 2.2.0 ;
+* Virtualbox 5.2.20 ;
+
+# Usage
+
+Launch or restart the Vagrant box :
+
+```
+./restart.sh
+```
+
+Grab a coffee and wait for the install. After that go to :
+
+* http://localhost:8080 : PowerDNS API ;
+* http://localhost:8081 : PowerDNS-admin GUI ;
+
+
+Configure the API key in PowerDNS-admin. The credentials are stored in `etc/`
--- powerdns/scripts/install.sh
+++ powerdns/scripts/install.sh
@@ -7,54 +7,71 @@ sudo -i
 # desactivate selinux
 setenforce 0
 # configure hostnames
-echo "
-192.168.0.10 powerdns01
-192.168.0.11 client01" >> /etc/hosts
+echo "192.168.0.10 powerdns01" >> /etc/hosts
 # desactivate firewall
 systemctl disable firewalld
-# install tools 
-yum install -y vim net-tools bind-utils epel-release yum-plugin-priorities git -y &&
-curl -o /etc/yum.repos.d/powerdns-auth-41.repo https://repo.powerdns.com/repo-files/centos-auth-41.repo
+# install repositories
+yum install -y \
+		epel-release \
+		yum-plugin-priorities \
+		https://centos7.iuscommunity.org/ius-release.rpm \
+		&& curl -o /etc/yum.repos.d/powerdns-auth-41.repo https://repo.powerdns.com/repo-files/centos-auth-41.repo \
+		&& curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
+# update the system
 yum update -y
+# install softwares
+yum install -y \
+		vim \
+		net-tools \
+		bind-utils \
+		git \
+		pdns \
+		pdns-backend-mysql \
+		mariadb-server \
+		mariadb \
+		python36u \
+		python36u-devel \
+		python36u-pip nginx \
+		gcc \
+		mariadb-devel \
+		mariadb-shared \
+		openldap-devel \
+		xmlsec1-devel \
+		xmlsec1-openssl \
+		libtool-ltdl-devel \
+		yarn \
+		-y
 
-if [[ $HOSTNAME =~ powerdns[0-9]+ ]]; then
-	# install mariadb backend
-	yum -y install mariadb-server mariadb 
-	systemctl enable mariadb.service
-	systemctl start mariadb.service
-	mysql -e "CREATE DATABASE ${DATABASE}";
-	mysql -D ${DATABASE} < /vagrant/scripts/pdns.sql
-	#mysql_secure_installation
-	yum -y install pdns pdns-backend-mysql
-	cp /vagrant/etc/pdns.conf /etc/pdns/pdns.conf
-	systemctl enable pdns.service
-	systemctl start pdns.service
+# launch database
+systemctl enable mariadb.service
+systemctl start mariadb.service
+mysql -e "CREATE DATABASE ${DATABASE}";
+mysql -D ${DATABASE} < /vagrant/scripts/pdns.sql
+#mysql_secure_installation <= mandatory for production use
 
-	# install webgui
-	yum install https://centos7.iuscommunity.org/ius-release.rpm -y
-	yum install python36u python36u-devel python36u-pip nginx -y
-	pip3.6 install -U pip
- 	pip install -U virtualenv
-	rm -f /usr/bin/python3 && ln -s /usr/bin/python3.6 /usr/bin/python3
-	yum install gcc mariadb-devel MariaDB-shared openldap-devel xmlsec1-devel xmlsec1-openssl libtool-ltdl-devel -y
-	curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
-	yum install yarn -y
-	git clone https://github.com/ngoduykhanh/PowerDNS-Admin.git /opt/web/powerdns-admin
-        cd /opt/web/powerdns-admin
-	virtualenv -p python3 flask
-	. ./flask/bin/activate
-	pip install python-dotenv
-	pip install -r requirements.txt
-	cp /vagrant/etc/config.py /opt/web/powerdns-admin
-	export FLASK_APP=app/__init__.py
-	flask db upgrade
-	yarn install --pure-lockfile
-	flask assets build
-	. ./flask/bin/activate
-	./run.py &
-	exit
-fi
+# launch DNS server
+cp /vagrant/etc/pdns.conf /etc/pdns/pdns.conf
+systemctl enable pdns.service
+systemctl start pdns.service
 
-if [[ $HOSTNAME =~ client[0-9]+ ]]; then
-
-fi
+# install webgui
+pip3.6 install -U pip
+pip install -U virtualenv
+rm -f /usr/bin/python3 && ln -s /usr/bin/python3.6 /usr/bin/python3
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
+git clone https://github.com/ngoduykhanh/PowerDNS-Admin.git /opt/web/powerdns-admin
+cd /opt/web/powerdns-admin
+virtualenv -p python3 flask
+. ./flask/bin/activate
+pip install python-dotenv
+pip install -r requirements.txt
+cp /vagrant/etc/config.py /opt/web/powerdns-admin
+export FLASK_APP=app/__init__.py
+flask db upgrade
+yarn install --pure-lockfile
+flask assets build
+# configure systemd
+cp /vagrant/etc/powerdns-admin.service /lib/systemd/system/
+systemctl enable powerdns-admin.service
+# launch GUI
+systemctl start powerdns-admin.service
